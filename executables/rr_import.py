6b10056601b3ac6fc08fffa3e382da6b8808d0a0
Jürgen Reiser <juergen.reiser@neo-q.de>
2021-06-21 20:47:11 +0200

    Refactor prompting

--- executables/rr_import.py
+++ executables/rr_import.py
@@ -6,69 +6,106 @@ class ImportCmd(cmd.Cmd):
     def __init__(self):
         super().__init__()
         self.executable = 'C:\\ProgramData\\RadioReport\\RadioReport.exe'
+        self.study = -1
+        self.study_instance_uids = {
+            1: '1.2.276.0.33.1.0.4.192.168.56.148.20200331.1192945.88622.2',
+            2: '1.2.276.0.18.14.200.2.0.0.2.20210415.90634.95.48',
+            3: '1.2.826.0.1.3680043.10.94.20210505100455',
+            4: '1.3.6.1.4.1.24930.2.64834516866074.1587242',
+            5: '1.2.276.0.18.14.200.2.0.0.2.20210420.155835.67.94',
+            6: '1.3.46.670589.5.2.10.2156913941.892665384.993397',
+            7: '',
+            8: ''
+        }
+        self.accession_numbers = {
+            1: 'KNI_0002',
+            2: '1344182',
+            3: 'KOB6DJV28C',
+            4: 'SCH_0001',
+            5: '1345229',
+            6: '',
+            7: '',
+            8: ''
+        }
+        self.patient_ids = {
+            1: 'KNI0001',
+            2: '370142',
+            3: '21',
+            4: 'SCH0001',
+            5: '43549',
+            6: '0009703828',
+            7: '',
+            8: ''
+        }
+        self.prompt_study()
+        self.prompt_variant()
+
+    def prompt_study(self):
+        input_string = input('''
+                Studie auswählen (Enter für Studie 1):
+                  1 - Knie^Routine Knie / Knie^Hilde^
+                  2 - MR LWS / Fachtan^Andreas
+                  3 - UNDEFINED / ROSE^RAPHAEL
+                  4 - Schulter_^Routine+ / Schulter^Peter^
+                  5 - MR Knie li. / Neubig^Detlef
+                  6 - HEAD EXP2 -> fehlende Pflichtfelder
+                  7 - 
+                  8 - 
+
+                Eingabe: ''')
+
+        if not input_string:
+            input_string = '1'
+        self.study = int(input_string)
+        if self.study < 1 or self.study > 6:
+            raise Exception('Ungültige Eingabe für Patient')
+
+    def prompt_variant(self):
         self.prompt = '''
-        CFind:
-          StudyInstanceUid only:
-            1 - Fachtan^Andreas
-            2 - Knie^Hilde^
-        
-          AccessionNumber only:
-            10 - Fachtan^Andreas
-            11 - Knie^Hilde^
-          
-          PatientId + StudyInstanceUid:
-            20 - Fachtan^Andreas
-            21 - Knie^Hilde^
-          
-          PatientId + AccessionNumber:
-            30 - Fachtan^Andreas
-            31 - Knie^Hilde^
-          
-          PatientId + StudyInstanceUid + AccessionNumber:
-            40 - Fachtan^Andreas
-            41 - Knie^Hilde^
-          
-          PatientId only:
-            50 - Fachtan^Andreas
-            51 - Knie^Hilde^
-            
-          Missing required fields:
-            60 - HEAD EXP2
-        
-        CMove:
-          StudyInstanceUid only:
-            100 - Fachtan^Andreas
-            101 - Knie^Hilde^
-            
-        Eingabe (exit zum Beenden): '''
+                Variante auswählen (exit zum Verlassen):
+                  1 - StudyInstanceUid only
+                  2 - AccessionNumber only
+                  3 - PatientId + StudyInstanceUid + AccessionNumber
+                  4 - PatientId + StudyInstanceUid
+                  5 - PatientId + AccessionNumber
+                  6 - PatientId only
+
+                Eingabe: '''
+
+    def do_1(self, _):
+        self.start_app(f'--studyInstanceUid={self.get_study_instance_uid()}')
+
+    def do_2(self, _):
+        self.start_app(f'--accessionNumber={self.get_accession_number()}')
+
+    def do_3(self, _):
+        self.start_app(f'--patientId={self.get_patient_id()}', f'--studyInstanceUid={self.get_study_instance_uid()}',
+                       f'--accessionNumber={self.get_accession_number()}')
+
+    def do_4(self, _):
+        self.start_app(f'--patientId={self.get_patient_id()}', f'--studyInstanceUid={self.get_study_instance_uid()}')
+
+    def do_5(self, _):
+        self.start_app(f'--patientId={self.get_patient_id()}', f'--accessionNumber={self.get_accession_number()}')
+
+    def do_6(self, _):
+        self.start_app(f'--patientId={self.get_patient_id()}')
 
     def do_exit(self, _):
         print('Tschüss')
         return True
 
-    def do_1(self, _):
-        self.start_app('--studyInstanceUid=1.2.276.0.18.14.200.2.0.0.2.20210415.90634.95.48')
-
-    def do_10(self, _):
-        self.start_app('--accessionNumber=1344182')
-
-    def do_20(self, _):
-        self.start_app('--patientId=370142', '--studyInstanceUid=1.2.276.0.18.14.200.2.0.0.2.20210415.90634.95.48')
-
-    def do_30(self, _):
-        self.start_app('--patientId=370142', '--accessionNumber=1344182')
-
-    def do_40(self, _):
-        self.start_app('--patientId=370142', '--studyInstanceUid=1.2.276.0.18.14.200.2.0.0.2.20210415.90634.95.48', '--accessionNumber=1344182')
-
-    def do_50(self, _):
-        self.start_app('--patientId=370142')
-
-    def do_60(self, _):
-        self.start_app('--studyInstanceUid=1.3.46.670589.5.2.10.2156913941.892665384.993397')
-
     def start_app(self, *params):
         subprocess.run([self.executable, *params])
+
+    def get_study_instance_uid(self):
+        return self.study_instance_uids[self.study]
+
+    def get_accession_number(self):
+        return self.accession_numbers[self.study]
+
+    def get_patient_id(self):
+        return self.patient_ids[self.study]
 
 
 if __name__ == "__main__":
